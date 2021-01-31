bf555c65e0725282d6ae350898d861b2c809d34e
Leon Schlote <leonschlote@gmail.com>
2021-01-31 10:02:10 +0100

    added meta inf and image sync for spotify

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const { exec } = require("child_process");
-
-const express = require('express'),
+const { exec } = require("child_process"),
+express = require('express'),
 app = express(),
 server = require('http').Server(app),
 io = require('socket.io')(server),
@@ -8,20 +7,31 @@ io = require('socket.io')(server),
 fs = require('fs'),
 httpPort = 8080
 
-var available_sources = require(__dirname+'/sources.json')
-var sources_html = ""
-var services = {}
+var available_sources = JSON.parse(fs.readFileSync(__dirname+'/sources.json', 'utf-8'))
 
-for (source of available_sources){
-	services[source.id] = source.service
-	sources_html += "<a class=\"source_container\" href=\"javascript:change_source('" + source.id + "')\"></a>"
+var sources_html = ""
+var info = {
+	'source_name': 'test_name',
+	'title': 'test_title',
+	'detail': 'detail - detail'
+}
+
+var active_source = ''
+var active_image = 'empty_album_art.png'
+
+for (id in available_sources){
+	let source = available_sources[id];
+	console.log(source)
+	sources_html += "<a class=\"source_container\" href=\"javascript:change_source('" + id + "')\" style=\"background-image: url('/static/img/"+id+".png'\"></a>"
 }
 
 
 var website = fs.readFileSync(__dirname + '/static/html/index.html').toString().replace('<$sources_list$/>',sources_html)
 
 app.use('/static/css', express.static(__dirname+'/static/css'))
+app.use('/static/img', express.static(__dirname+'/static/img'))
 app.use('/static/js', express.static(__dirname+'/static/js'))
+
 
 app.get('/', (req, res)=>{
 	  res.send(website)
@@ -32,29 +42,80 @@ app.get('*', (req, res)=>{
 })
 
 io.on('connection', (socket)=>{
-	socket.on('change_source', (source_id)=>{
-		console.log(source_id)
-		for(source of available_sources){
-			//console.log(source.id + " <- source")
-			if (source.id != source_id){
-				exec('systemctl stop ' + source.service)
-			}
-		}
-		setTimeout(()=>{exec('systemctl start ' + services[source_id])},2000)
-	          })
-	          })
+	socket.on('change_source', (id)=>{
+		enable_source(id)
+      })
+})
+
+var update_info_interval = setInterval(()=>{
+	if(available_sources[active_source] !== undefined){
+		io.emit('update_info',available_sources[active_source].info)
+	}
+},2000)
 
 server.listen(httpPort, ()=>{
 	  console.log('HTTP Server started, listening on Port ' + (httpPort))
 })
 
+function enable_source(id){
+	console.log('Enabling Audio Source -> ' + id)
+
+	if(available_sources[id] !== undefined){
+		active_source = id
+		for(loop_id in available_sources){
+			let source = available_sources[id]
+			console.log(source)
+			if (loop_id != id){
+				console.log("Stopping -> " + available_sources[loop_id].service)
+				exec('systemctl stop ' + available_sources[loop_id].service)
+		        }
+	      }
+              exec('systemctl start ' + available_sources[id].service)
+
+	}
+}
+
+function set_active_image(src){
+	io.emit('update_album_art', src)
+}
 
 
+function makeid(length) {
+	   var result           = '';
+	   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	   var charactersLength = characters.length;
+	   for ( var i = 0; i < length; i++ ) {
+		         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		      }
+	   return result;
+}
 
+// =========== Shairport update metadata ============
+const ShairportReader = require('shairport-sync-reader')
+var pipeReader = new ShairportReader({ path: '/tmp/shairport-sync-metadata' });
+var airplay_album_art = '';
 
+pipeReader.on('meta', (data)=>{
+	//console.log(data)
+	if(data.asal != "" && data.asal != undefined)
+		available_sources['airplay'].info.title = data.asal
+	if(data.asar != "" && data.asar != undefined && data.minm != "" && data.minm != undefined)
+		available_sources['airplay'].info.detail = data.asar + ' - ' + data.minm
+})
 
-/*
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);*/
+pipeReader.on('PICT', (data)=>{
+
+	var nocache = makeid(5) 
+	airplay_album_art = data
+
+	fs.writeFileSync('static/img/airplay_album_art_' + nocache + '.png', data);
+
+	
+	app.get('/static/img/airplay_album_art_' + nocache + '.png', (req, res)=>{
+		  res.send(airplay_album_art)
+	})
+
+	set_active_image('airplay_album_art_' + nocache + '.png')
+	console.log(app._router.stack)
+})
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,28 @@
 var host = window.location.hostname + ":" + window.location.port
+var current_info = {};
 const socket = io(host);
 console.log('Connecting to WebSocket at '+host);
 
-socket.on('update_info', (info)=>{
-	        console.log(info)
-	      })
+socket.on('update_info', (new_info)=>{
+	if(new_info.source_name != current_info.source_name)
+		$('.active_source .source_name').html(new_info.source_name)
+	if(new_info.title != current_info.title)
+		$('.active_source .title').html(new_info.title)
+	if(new_info.detail != current_info.detail)
+		$('.active_source .detail').html(new_info.detail)
+})
 
 socket.on('update_active', (info)=>{
 	        console.log(info)
 	      })
 
+socket.on('update_album_art', (src)=>{
+	src = '/static/img/'+src
+	console.log(src)
+	$('.active_source').css('background',"linear-gradient(rgba(0, 0, 0, 0) 70%, rgba(0, 0, 0, 0.8)), url('" + src + "')")
+})
+
 function change_source(source_id){
-	alert(source_id)
 	socket.emit('change_source', source_id)
 }
+
